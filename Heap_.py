e45b9076cfe391a3eb2dd26a7020fa5210d66620
arbazsheik <arbaz.sheik@jotter.ai>
2018-12-19 23:34:01 +0530

    Heap done!

--- Heap_.py
+++ Heap_.py
@@ -1,38 +1,42 @@
-def left_(i):
+def get_left(i):
 	return 2*i
-def right_(i):
+def get_right(i):
 	return 2*i+1
 
 def max_heapify(a, i, n):
 	largest = i
-	left = left_(i) 
-	right = right_(i)
+	left = get_left(i) 
+	right = get_right(i)
 	if left<=n and a[left]>a[largest]:
 		largest = left
 	if right<=n and a[right]>a[largest]:
 		largest = right
 	if largest!=i:
 		a[largest], a[i]= a[i], a[largest]
-		# print(a)
-		a = max_heapify(a,largest,n)
-	return a
+		max_heapify(a,largest,n)
 def build_heap(a, n):
 	for i in range(n//2,0,-1):
-		a = max_heapify(a, i, n)
-	return a
+		max_heapify(a, i, n)
+		print(a)
 
 def heap_sort(a,n):
-	for i in range(n,2,-1):
+	for i in range(n,1,-1):
 		a[i], a[1] = a[1], a[i]
 		n-=1
-		a = max_heapify(a,1,n)
-	return a
+		max_heapify(a,1,n)
+
+
 arr = list(map(int, input().strip().split()))
 n = len(arr)
 arr.insert(0,0)
-print('before',arr)
-arr = max_heapify(arr, 1, n)
-print('last:',arr)
-print(build_heap(arr,n)[1:])
-print(heap_sort(arr, n)[1:])
 print(arr)
+
+print('build_heap------')
+build_heap(arr,n)
+print(arr)
+
+print('heap_sort------')
+heap_sort(arr, n)
+print(arr)
+
+print(n)
